a0f500f1e92e502b5550e6b76c8bc88296fa35fe
mk1808 <monikor97@gmail.com>
2019-05-11 12:13:15 +0200

    #2 dodanie nagłówka

--- recruitment-app/src/app/app.module.ts
+++ recruitment-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from './items/home.component';
+import { CardComponent } from './items/card/card.component';
+import { CardDetailsComponent } from './items/card-details/card-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardComponent,
+    CardDetailsComponent
   ],
   imports: [
     BrowserModule,
