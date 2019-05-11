6065dce87d8ad39bb2bd3ba799103c86a5fad632
mk1808 <monikor97@gmail.com>
2019-05-11 20:02:48 +0200

    #3 dodanie routingu

--- recruitment-app/src/app/app-routing.module.ts
+++ recruitment-app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CardDetailsComponent } from './home/card-details/card-details.component';
 
 const routes: Routes = [
-  
+  { path: 'home', component: HomeComponent},
+ { path: 'home/:id',  component: HomeComponent},
+ 
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/home'},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
